4a47e3f119a413d695bc59299029cb63b7221fef
Marco <me.kong@gmx.de>
2018-06-23 12:32:31 +0200

    Design und Optik. Va bei Name. weitere folgen

--- src/com/game/src/main/Menu/Menu.java
+++ src/com/game/src/main/Menu/Menu.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 
 public class Menu extends MenuElement{
 
-    private Rectangle playButton = new Rectangle(Game.WIDTH /2 -90, 400, 180,50);
-    private Rectangle scoreButton = new Rectangle(Game.WIDTH /2 -90, 500, 280,50);
-    private Rectangle quitButton = new Rectangle(Game.WIDTH /2 -90, 600, 180,50);
+    private Rectangle playButton = new Rectangle(Game.WIDTH /2 -300, 550, 130,50);
+    private Rectangle scoreButton = new Rectangle(Game.WIDTH /2 -90, 550, 180,50);
+    private Rectangle quitButton = new Rectangle(Game.WIDTH /2 +170, 550, 130,50);
     private static BufferedImage background = null;
 
 
@@ -33,18 +33,18 @@ public class Menu extends MenuElement{
 
         g.drawImage(background,0,0, Game.WIDTH, Game.HEIGHT,null);
 
-        Font fnt1 = new Font("Gill Sans", Font.BOLD, 50);
+        Font fnt1 = new Font("Gill Sans", Font.BOLD, 70);
         g.setFont(fnt1);
-        g.setColor(Color.ORANGE);
-        g.drawString("COOLES RENN SPEIL", Game.WIDTH/2 - 280, 300);
+        g.setColor(Color.white);
+        g.drawString("COOLES RENN SPEIL", Game.WIDTH/2 - 400, 100);
 
         Font fnt2 = new Font("arial", Font.BOLD, 30);
         g.setFont(fnt2);
-        g.drawString("Play", playButton.x+30, playButton.y+32);
+        g.drawString("Play", playButton.x+30, playButton.y+35);
         g2d.draw(playButton);
-        g.drawString("Highscore", scoreButton.x+19, scoreButton.y+32);
+        g.drawString("Highscore", scoreButton.x+19, scoreButton.y+35);
         g2d.draw(scoreButton);
-        g.drawString("Quit", quitButton.x+19, quitButton.y+32);
+        g.drawString("Quit", quitButton.x+30, quitButton.y+35);
         g2d.draw(quitButton);
     }
 }
